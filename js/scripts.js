53046f53e040f36e83e870903d1827d0672e5904
Sean Holstrom <student@epicodus.com>
2015-02-20 10:52:50 -0800

    Add order to display pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,21 @@ var Pizza = {
     return parseInt(area / this.sliceArea);
   }
 };
+
+$(document).ready(function() {
+  $("form#new-pizza").submit(function(event) {
+    event.preventDefault();
+
+    var newSize = parseInt($("input#size").val());
+    var newTopping = $("[name=topping]:checked").val();
+    var newSliceArea = parseInt($("input#slice-area").val());
+    var newPizza = Object.create(Pizza);
+    newPizza.size = newSize;
+    newPizza.topping = newTopping;
+    newPizza.sliceArea = newSliceArea;
+
+    $(".order").append("<li>" + newPizza.size + " inch " + newPizza.topping + " pizza containing " + newPizza.findSlices() + " slices</li>");
+    $(".order").show("slow");
+
+  });
+});
